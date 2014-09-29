205a7dae2d3412b9dc53ad70945996ec13e54e42
Anuj Katiyal <anujk3@gmail.com>
2014-09-29 12:51:20 +0530

    meaning of the static variable

--- DayTenApp/MySingleton.m
+++ DayTenApp/MySingleton.m
@@ -16,7 +16,11 @@
 
 @implementation MySingleton
 
-static MySingleton *singletonInstance; // why use the word static, keeps the scope of this variable limited to this file only like in C
+static MySingleton *singletonInstance; 
+
+/*
+In both C and Objective-C, a static variable is a variable that is allocated for the entire lifetime of a program. This is in contrast to automatic variables, whose lifetime exists during a single function call; and dynamically-allocated variables like objects, which can be released from memory when no longer used. More simply put, a static variable's value is maintained throughout all function/method calls. When declared outside of a function, a static variable is visible to everything within the file in which it is declared; when declared inside a function or method, it is visible only within that function or method, but the value is retained between calls.
+*/
 
 + (MySingleton *)getInstance{
     if (singletonInstance == nil){
